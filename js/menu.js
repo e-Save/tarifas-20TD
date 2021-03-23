d3aeac3977a04c1f55d3e17980172a93cdbeee4f
Omar Alarcon <omar98alarcon@gmail.com>
2021-03-23 08:59:30 +0100

    Prueba menu header, estilo títulos - Omar - 23/03/2021

--- js/menu.js
+++ js/menu.js
@@ -1,15 +1,12 @@
-$(window).ready(function() {
-  var show_collapse = true;
-  console.log(show_collapse)
-  $('.navbar-toggler').on('click', function(){
-    console.log(show_collapse)
-    if (show_collapse){
-      $('.collapse').addClass('show');
-      show_collapse = false;
-    }else{
-      $('.collapse').removeClass('show');
-      show_collapse = true;
-    };
-  });
-});
+
+var show_collapse = true;
+function show_menu(){
+  if (show_collapse){
+    $('.collapse').addClass('show');
+    show_collapse = false;
+  }else{
+    $('.collapse').removeClass('show');
+    show_collapse = true;
+  };
+};
 
