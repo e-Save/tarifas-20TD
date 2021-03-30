34ba238aac9bea389bff8dceb2133943e1594e36
Tomas Higuera <tomas.higuera@electric-save.com>
2021-03-30 10:14:01 +0200

    Change colo transition. Tomas Higuera. 30-3-21

--- js/formula.js
+++ js/formula.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
     if (jQuery(window).width() > 768) {
         $('.opr1').hover(function(event){
             $('.desc-opr1').css("bottom", "50px");
-            $('.desc-opr1').css("color", "black");
+            $('.desc-opr1').css("color", "white");
         }, function(){
             $('.desc-opr1').css("bottom", "0");
             $('.desc-opr1').css("color", "transparent");
@@ -12,7 +12,7 @@ $(document).ready(function(){
 
         $('.opr2').hover(function(event){
             $('.desc-opr2').css("bottom", "50px");
-            $('.desc-opr2').css("color", "black");
+            $('.desc-opr2').css("color", "white");
         }, function(){
             $('.desc-opr2').css("bottom", "0");
             $('.desc-opr2').css("color", "transparent");
@@ -20,7 +20,7 @@ $(document).ready(function(){
 
         $('.opr3').hover(function(event){
             $('.desc-opr3').css("bottom", "50px");
-            $('.desc-opr3').css("color", "black");
+            $('.desc-opr3').css("color", "white");
         }, function(){
             $('.desc-opr3').css("bottom", "0");
             $('.desc-opr3').css("color", "transparent");
@@ -28,7 +28,7 @@ $(document).ready(function(){
 
         $('.opr4').hover(function(event){
             $('.desc-opr4').css("bottom", "50px");
-            $('.desc-opr4').css("color", "black");
+            $('.desc-opr4').css("color", "white");
         }, function(){
             $('.desc-opr4').css("bottom", "0");
             $('.desc-opr4').css("color", "transparent");
